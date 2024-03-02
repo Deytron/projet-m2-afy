0f7641ce5071e5fd6c28987c933aa6f4925399cd
Deytron <hyperpikaevo@gmail.com>
2024-03-02 11:11:48 +0100

    switched to Gin  for web

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// HTML Location
+	r.LoadHTMLFiles("html/index.html")
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+
+	r.Run(":8081")
+}
