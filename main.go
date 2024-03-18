b8ae6f187a165fe33bb49123cc3b4d95588116d7
Deytron <hyperpikaevo@gmail.com>
2024-03-18 17:08:48 +0100

    added navbar js

--- main.go
+++ main.go
@@ -14,6 +14,8 @@ func main() {
 
 	r.StaticFile("/logo-light.png", "./assets/logo-light.png")
 	r.StaticFile("/style.css", "./style/style.css")
+	r.StaticFile("/bulma-carousel.min.css", "./bulma-carousel/dist/css/bulma-carousel.min.css")
+	r.StaticFile("/bulma-carousel.min.js", "./bulma-carousel/dist/js/bulma-carousel.min.js")
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{})
